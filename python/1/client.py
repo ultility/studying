3f9670c8f49d95c5d8f3bbf76c651e3e8b559ded
ultility <56175528+ultility@users.noreply.github.com>
2021-12-02 19:32:41 +0200

    Update client.py

--- python/1/client.py
+++ python/1/client.py
@@ -2,24 +2,36 @@ import socket
 import sys
 
 from pynput.keyboard import Key, Listener
-HOST = sys.argv[1]
-PORT = int(sys.argv[2])
+
 
 def key_press(key):
+    global closed
     if key == Key.esc:
-        s.close()
-        global closed
         closed = True
-    else:
+        s.close()
+    elif not closed:
         k = str(key)
         k = k.replace("'", "")
         s.sendall(bytes(k, "ascii"))
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+def main():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
+    global closed
     closed = False
     listener = Listener(on_press=key_press)
     listener.start()
     while not closed:
-        data = s.recv(1024)
-        print(data.decode("ASCII"))
+        try:
+            data = s.recv(1024)
+            print(data.decode("ASCII"))
+            if len(data) == 0:
+                break
+        except Exception as e:
+            print(e)
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+global s
+closed = True
+main()
