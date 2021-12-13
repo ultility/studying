cfa503703617545945cc446e5e50b438c0695330
ultility <56175528+ultility@users.noreply.github.com>
2021-12-13 18:13:41 +0200

    Update main.py

--- python/2/main.py
+++ python/2/main.py
@@ -1,1 +1,26 @@
+import stat
+import sys
+import os
+
+def main():
+    path = sys.argv[1]
+    try:
+        if os.path.isfile(path):
+            file = open(path, "r")
+            print(file.read())
+        elif os.path.isdir(path):
+            files = os.listdir(path)
+            print(files)
+        permissions = os.stat(path)[stat.ST_MODE]
+        printPerms(permissions)
+    except FileNotFoundError:
+        print("file at " + path + " does not exist")
+
+def printPerms(perms):
+    print("readable: " + str(bool(perms & stat.S_IRGRP)))
+    print("writeable: " + str(bool(perms & stat.S_IWGRP)))
+
+if __name__ == "__main__":
+    main()
+
 
