7d2513b37596f9dbbb8cb4270fa09604e42c576c
Ori Nimron <56004200+orinimron123@users.noreply.github.com>
2021-12-27 21:01:09 +0200

    Update webserver.h

--- cpp/webserver.h
+++ cpp/webserver.h
@@ -1,4 +1,11 @@
+// CR: When writing C++ code, the header file extension should be hpp.
+
 #include <string>
+// CR: pragma once should be the first thing in h file.
 #pragma once
+// CR: Move this function declaration to StringUtils.hpp
+// CR: When declaring a function, also mention the names of the arguments, so other people can understand when are the arguments mean. Here I can't understand what is the string, why do we char an then int.
 int replace_all(std::string, const char*, int, const char*);
+
+// CR: Is this socket related? Move it to Socket class.
 void send_message(int, const char path[]);
